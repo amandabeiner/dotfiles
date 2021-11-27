ca31254f614ec03db974c7f4dbe5623fcca8cf55
Amanda Beiner <aebeiner@gmail.com>
2021-11-27 15:42:39 -0500

    refactor to be more readable

--- install.sh
+++ install.sh
@@ -4,52 +4,56 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-fancy_echo() {
-  local fmt="$1"; shift
+function fancy_echo() {
+        local fmt="$1"; shift
 
-  printf "\\n$fmt\\n" "$@"
+        printf "\\n$fmt\\n" "$@"
 }
 
-get() {
-  curl -fLO $1 --create-dirs $2
+function apt_get_packages() {
+        fancy_echo "Installing apt-get packages"
+        apt-get -y install fish fzf ripgrep git kitty
 }
+
+function install_starship() {
+        fancy_echo "Installing Starship"
+        curl -o starship-install.sh -fsSL https://starship.rs/install.sh
+        sudo bash starship-install.sh -y
+        ln -sf $(pwd -P)/.config/starship.toml $HOME/.config/starship.toml
+
+}
+
+function copy_dotfiles() {
+        fancy_echo "Installing dotfiles"
+        locals=("fish" "vim" "starship.toml")
+
+        ln -sf $(pwd -P)/.config/fish $HOME/.config/fish
+        ln -sf $(pwd -P)/.config/vim $HOME/.config/vim
+}
+
+function change_shell() {
+        fancy_echo "Switching to fish shell"
+        chsh -s $(which fish)
+}
+
+function install_vim_plugins() {
+        fancy_echo "Installing vim plugins"
+
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        +PlugInstall +PlugClean! +qa
+}
+
 
 if [ "$CODESPACES" == "true" ]; then
-        fancy_echo "In codespaces! Installing apt-get packages"
-        apt-get -y install fish fzf ripgrep git kitty
+        fancy_echo "In codespaces!"
 
-        # fancy_echo "Installing asdf"
-        # git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
-        fancy_echo "Installing Starship"
-        sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-
-
-
-        fancy_echo "Installing dotfiles"
-        mv $HOME/.gitconfig $HOME/.gitconfig.old
-
-        locals=("fish" "vim" "starship.toml")
-        for i in "${locals[@]}"
-        do
-                ln -sf $(pwd -P)/.config/$i "$HOME/dotfiles"
-        done
- 
-        fancy_echo "Switching to fish"
-        chsh -s $(which fish)
-
-        fancy_echo "Installing vim plugins"
-        if [ -e "$HOME"/.vim/autoload/plug.vim ]; then
-                vim -E -s +PlugUpgrade +qa
-        else
-                curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-                    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-        fi
-        mkdir ~/.vim-tmp # add vim backup directory to prevent errors like https://stackoverflow.com/questions/8428210/cannot-create-backup-fileadd-to-overwrite
-        vim +PlugUpdate +PlugClean! +qa
+        apt_get_packages
+        install_starship
+        copy_dotfiles
+        change_shell
+        install_vim_plugins
 
         fancy_echo "All done"
-else
-        fancy_echo "Not running in a codespace"
-
 fi
 
