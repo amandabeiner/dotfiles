510b62e9d17bc45853bf3d1d54646409db4927c0
Amanda Beiner <aebeiner@gmail.com>
2023-06-15 13:01:26 -0400

    try different install script

--- install.sh
+++ install.sh
@@ -1,51 +1,71 @@
 #!/bin/bash
-
 exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-function fancy_echo() {
-        local fmt="$1"; shift
+#Symlink dotfiles
+echo "Symlinking dotfiles"
+ln -sf $(pwd -P)/.config/.* $HOME/
 
-        printf "\\n$fmt\\n" "$@"
-}
+# Install Plug and plugins
+echo "Installing plugins"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -es -u ~/.vimrc +PlugInstall +qa
+
+# Install ctags
+echo "Installing ctags"
+ctags -R --exclude=.git--exclude=vendor --exclude=node_modules --exclude=db --exclude=log .
+
+# Install brew dependencies
+echo "Installing brew dependencies"
+brew bundle --file $HOME/Brewfile
+
+# Changing to fish shell
+sudo chsh -s $(which fish) $(whoami)
+
+git config --global core.editor "vim"
 
 
-function copy_dotfiles() {
-        fancy_echo "Installing dotfiles"
-        mkdir -p $HOME/.config
+# exec > >(tee -i $HOME/dotfiles_install.log)
+# exec 2>&1
+# set -x
 
-        ln -sf $(pwd -P)/.config/fish $HOME/.config/fish
-        ln -sf $(pwd -P)/.config/vim $HOME/.config/vim
-        ln -sf $(pwd -P)/.config/kitty $HOME/.config/kitty
-        ln -sf $(pwd -P)/.config/tmux $HOME/.config/tmux
-        ln -sf $(pwd -P)/.config/starship.toml $HOME/.config/starship.toml
-        ln -sf $(pwd -P)/.config/Brewfile $HOME/.config/Brewfile
-}
+# function fancy_echo() {
+#         local fmt="$1"; shift
 
-function change_shell() {
-        fancy_echo "Switching to fish shell"
-        sudo chsh -s $(which fish) $(whoami)
-}
+#         printf "\\n$fmt\\n" "$@"
+# }
 
-function install_vim_plugins() {
-        fancy_echo "Installing vim plugins"
-        vim -Es -u $HOME/.config/vim/.vimrc -c "PlugInstall | qa"
-}
 
-function brew_bundle() {
-  fancy_echo "Installing brew dependencies"
-  brew bundle --file $HOME/.config/Brewfile
-}
+# function copy_dotfiles() {
+#         fancy_echo "Installing dotfiles"
 
-if [ "$CODESPACES" == "true" ]; then
-        fancy_echo "In codespaces!"
-        copy_dotfiles
-        brew_bundle
-        change_shell
-        install_vim_plugins
+# }
 
-        git config --global core.editor "vim"
+# function change_shell() {
+#         fancy_echo "Switching to fish shell"
+#         sudo chsh -s $(which fish) $(whoami)
+# }
 
-        fancy_echo "All done"
-fi
+# function install_vim_plugins() {
+#         fancy_echo "Installing vim plugins"
+#         curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#         vim -es -u ~/.vimrc +PlugInstall +qa
+# }
+
+# function brew_bundle() {
+#   fancy_echo "Installing brew dependencies"
+#   brew bundle --file $HOME/Brewfile
+# }
+
+# if [ "$CODESPACES" == "true" ]; then
+#         fancy_echo "In codespaces!"
+#         copy_dotfiles
+#         brew_bundle
+#         install_vim_plugins
+#         change_shell
+
+#         git config --global core.editor "vim"
+
+#         fancy_echo "All done"
+# fi
